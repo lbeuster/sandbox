a8016927a97044c225cdecd080e858363b8800ee
lbeuster <Lars.Beuster@zanox.com>
2013-04-21 16:16:36 +0200

    test

--- sandbox-java7/src/test/java/de/lbe/sandbox/java7/_08DiamondOperatorTest.java
+++ sandbox-java7/src/test/java/de/lbe/sandbox/java7/_08DiamondOperatorTest.java
@@ -13,14 +13,13 @@ import org.junit.Test;
  */
 public class _08DiamondOperatorTest extends Assert {
 
-    /**
+	/**
      * 
      */
-    @Test
-    public void testDiamondOperator() {
-        // @NEW Wir koennen die generischen Typen weglassen
-        Map<String, List<String>> map = new HashMap<>();
-        assertNotNull(map);
-    }
-
+	@Test
+	public void testDiamondOperator() {
+		// @NEW Wir koennen die generischen Typen weglassen
+		Map<String, List<String>> map = new HashMap<>();
+		assertNotNull(map);
+	}
 }
