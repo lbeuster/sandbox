79958c27bc9f304e76a3f256c3fe526d51c76d9c
Lars Beuster <Lars.Beusterr@axelspringer.de>
2013-08-28 18:04:41 +0200

    Mongo

---
+++ sandbox-mongodb/src/main/java/de/lbe/sandbox/mongodb/MongoDriverMain.java
@@ -0,0 +1,52 @@
+package de.lbe.sandbox.mongodb;
+
+import java.util.Set;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
+/**
+ * Orika says that we should use the mapper factory as a singleton.
+ * 
+ * http://orika-mapper.github.io/orika-docs/performance-tuning.html
+ * 
+ * @author lars.beuster
+ */
+public class MongoDriverMain {
+
+	private static final String TESTDB = "larsdb";
+
+	public static void main(String[] args) throws Exception {
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+		// delete the test db
+		DB db = mongoClient.getDB(TESTDB);
+		mongoClient.dropDatabase(TESTDB);
+
+		Set<String> tableNames = db.getCollectionNames();
+		for (String s : tableNames) {
+			System.out.println(s);
+		}
+
+		DBCollection testData = db.getCollection("testTable");
+		BasicDBObject doc = new BasicDBObject("name", "MongoDB").append("type", "database").append("count", 1)
+				.append("info", new BasicDBObject("x", 203).append("y", 102));
+		testData.insert(doc);
+
+		BasicDBObject query = new BasicDBObject("count", new BasicDBObject("$gt", 0));
+		DBCursor cursor = testData.find(query);
+		try {
+			while (cursor.hasNext()) {
+				System.out.println(cursor.next());
+			}
+		} finally {
+			cursor.close();
+		}
+
+		System.out.println(testData.getCount());
+
+	}
+}
