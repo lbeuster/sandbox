9261fdcc9181dd417319a0238a65ea154c8a37e8
Lars Beuster <Lars.Beusterr@axelspringer.de>
2013-08-28 18:04:41 +0200

    Mongo

---
+++ sandbox-mongodb/src/main/java/de/lbe/sandbox/mongodb/morphia/Employee.java
@@ -0,0 +1,170 @@
+package de.lbe.sandbox.mongodb.morphia;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
+import com.google.code.morphia.Key;
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Id;
+import com.google.code.morphia.annotations.Indexed;
+import com.google.code.morphia.annotations.NotSaved;
+import com.google.code.morphia.annotations.PostLoad;
+import com.google.code.morphia.annotations.Property;
+import com.google.code.morphia.annotations.Reference;
+import com.google.code.morphia.annotations.Transient;
+import com.mongodb.DBObject;
+import com.zanox.lib.commons.lang.ObjectUtils;
+
+@Entity("employees")
+public class Employee {
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+
+	public Key<Employee> getManager() {
+		return manager;
+	}
+
+	public void setManager(Key<Employee> manager) {
+		this.manager = manager;
+	}
+
+	public List<Employee> getUnderlings() {
+		return underlings;
+	}
+
+	public void setUnderlings(List<Employee> underlings) {
+		this.underlings = underlings;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getReadButNotStored() {
+		return readButNotStored;
+	}
+
+	public void setReadButNotStored(String readButNotStored) {
+		this.readButNotStored = readButNotStored;
+	}
+
+	public int getNotStored() {
+		return notStored;
+	}
+
+	public void setNotStored(int notStored) {
+		this.notStored = notStored;
+	}
+
+	public boolean isStored() {
+		return stored;
+	}
+
+	public void setStored(boolean stored) {
+		this.stored = stored;
+	}
+
+	// auto-generated, if not set (see ObjectId)
+	@Id
+	private ObjectId id;
+
+	// value types are automatically persisted
+	private String firstName, lastName;
+
+	// only non-null values are stored
+	private Long salary = null;
+
+	// by default fields are @Embedded
+	// Address address;
+
+	// references can be saved without automatic loading
+	private Key<Employee> manager;
+
+	// refs are stored**, and loaded automatically
+	@Reference
+	private List<Employee> underlings = new ArrayList<Employee>();
+
+	// fields can be renamed
+	@Property("started")
+	private Date startDate;
+
+	@Property("left")
+	private Date endDate;
+
+	// fields can be indexed for better performance
+	@Indexed
+	private boolean active = false;
+
+	// fields can loaded, but not saved
+	@NotSaved
+	private String readButNotStored;
+
+	// fields can be ignored (no load/save)
+	@Transient
+	private int notStored;
+
+	// not @Transient, will be ignored by Serialization/GWT for example.
+	private transient boolean stored = true;
+
+	// Lifecycle methods -- Pre/PostLoad, Pre/PostPersist...
+	@PostLoad
+	void postLoad(DBObject dbObj) {
+		System.out.println(getClass().getSimpleName() + "#postLoad()");
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String toString() {
+		return ObjectUtils.dumpViaReflection(this);
+	}
+}
---
+++ sandbox-mongodb/src/main/java/de/lbe/sandbox/mongodb/morphia/MorphiaMain.java
@@ -0,0 +1,71 @@
+package de.lbe.sandbox.mongodb.morphia;
+
+import java.util.Collection;
+
+import com.google.code.morphia.Datastore;
+import com.google.code.morphia.Key;
+import com.google.code.morphia.Morphia;
+import com.mongodb.MongoClient;
+import com.zanox.lib.commons.util.CollectionUtils;
+
+/**
+ * Orika says that we should use the mapper factory as a singleton.
+ * 
+ * http://orika-mapper.github.io/orika-docs/performance-tuning.html
+ * 
+ * @author lars.beuster
+ */
+public class MorphiaMain {
+
+	private static final String TESTDB = "morphiadb";
+
+	public static void main(String[] args) throws Exception {
+
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+		// delete the test db
+		mongoClient.dropDatabase(TESTDB);
+
+		Morphia morphia = new Morphia();
+
+		Datastore ds = morphia.createDatastore(mongoClient, TESTDB);
+		morphia.map(Employee.class);
+
+		System.out.println(ds.getCount(Employee.class));
+
+		Employee boss = new Employee();
+		boss.setFirstName("boss1");
+		boss.setLastName("boss-lastname");
+		Key<Employee> bossId = ds.save(boss);
+
+		// get an employee without a manager
+		Employee scott = new Employee();
+		scott.setFirstName("scott");
+		scott.setLastName("scott-lastname");
+		scott.setManager(bossId);
+		Key<Employee> scottId = ds.save(scott);
+
+		Collection<Employee> emps = CollectionUtils.toList(ds.find(Employee.class).field("manager").notEqual(null).iterator());
+		System.out.println(emps);
+
+		boss = ds.getByKey(Employee.class, bossId);
+		System.out.println(boss.getUnderlings());
+
+		boss.getUnderlings().add(scott);
+		ds.save(boss);
+
+		boss = ds.getByKey(Employee.class, bossId);
+		System.out.println(boss.getUnderlings());
+
+		/*
+		 * Key<Employee> scottsKey = ds.save(new Employee("Scott", "Hernandez", ds.getKey(boss), 150**1000));
+		 * 
+		 * //add Scott as an employee of his manager UpdateResults<Employee> res = ds.update( boss,
+		 * ds.createUpdateOperations(Employee.class).add("underlings", scottsKey) );
+		 * 
+		 * // get Scott's boss; the same as the one above. Employee scottsBoss = ds.find(Employee.class).filter("underlings", scottsKey).get();
+		 * 
+		 * for (Employee e : ds.find(Employee.class, "manager", boss)) print(e);
+		 */
+	}
+}
