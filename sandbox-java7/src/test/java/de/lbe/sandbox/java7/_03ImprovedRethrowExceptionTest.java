531aa2420bd630a007f4879af60c39890fef0916
Lars Beuster <Lars.Beusterr@axelspringer.de>
2013-09-18 17:59:52 +0200

    TomEE

--- sandbox-java7/src/test/java/de/lbe/sandbox/java7/_03ImprovedRethrowExceptionTest.java
+++ sandbox-java7/src/test/java/de/lbe/sandbox/java7/_03ImprovedRethrowExceptionTest.java
@@ -15,25 +15,24 @@ import de.asideas.lib.commons.io.IOUtils;
  */
 public class _03ImprovedRethrowExceptionTest extends Assert {
 
-    /**
+	/**
      * 
      */
-    @Test(expected = FileNotFoundException.class)
-    public final void testRethrow()
-        throws FileNotFoundException {
-    	InputStream in = null;
-        try {
-            in = new FileInputStream(new File("invalid"));
-            fail("non-existing file " + in);
-        } catch (final Throwable ex) {
-            // @NEW
-            // wir koennen Throwable abfangen und weiterwerfen - die deklarierte Exception unserer Methode kann ruhig
-            // FileNotFoundException bleiben. Der Compiler geht davon aus, dass unsere Methode nur Exceptions werfen
-            // kann, die auch im try-Block geworfen werden koennen
-            throw ex;
-        } finally {
-        	IOUtils.closeQuietly(in);
-        }
-    }
+	@Test(expected = FileNotFoundException.class)
+	public final void testRethrow() throws FileNotFoundException {
+		InputStream in = null;
+		try {
+			in = new FileInputStream(new File("invalid"));
+			fail("non-existing file " + in);
+		} catch (Throwable ex) {
+			// @NEW
+			// wir koennen Throwable abfangen und weiterwerfen - die deklarierte Exception unserer Methode kann ruhig
+			// FileNotFoundException bleiben. Der Compiler geht davon aus, dass unsere Methode nur Exceptions werfen
+			// kann, die auch im try-Block geworfen werden koennen
+			throw ex;
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+	}
 
 }
---
+++ sandbox-jee6-webapp/src/test/java/de/lbe/jee6/webapp/WarArquillianTest.java
@@ -0,0 +1,44 @@
+package de.lbe.jee6.webapp;
+
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+
+import de.asideas.lib.commons.arquillian.AbstractJUnit4ArquillianTest;
+import de.asideas.lib.commons.shrinkwrap.ShrinkWrapUtils;
+
+/**
+ * @author lars.beuster
+ */
+public class WarArquillianTest extends AbstractJUnit4ArquillianTest {
+
+//	@ArquillianResource
+//	private URL contextPathURL;
+
+	/**
+	 * 
+	 */
+	@Deployment(testable = false)
+	public static WebArchive deployment() {
+		WebArchive archive = ShrinkWrap.create(WebArchive.class, "test-webapp.war").addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+		ShrinkWrapUtils.addDirectory(archive, "src/main/webapp");
+		return archive;
+	}
+
+	/**
+	 * In contrast to JBoss we can use the complete test-classpath we we are inside the container. With JBoss we only have the module-classloader
+	 * without the test-classpath.
+	 * 
+	 * TomEE doesn't deploy Jars as BeanArchives (by default?).
+	 */
+	@Test
+	public void testStartup() throws Exception {
+//		System.out.println(this.contextPathURL);
+
+	}
+}
