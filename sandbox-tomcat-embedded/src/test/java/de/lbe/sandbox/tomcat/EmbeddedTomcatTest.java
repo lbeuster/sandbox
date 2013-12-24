eb5f4a5dc072ef7fb2db228adc40c4054c11d95e
lbeuster <Lars.Beuster@web.de>
2013-12-24 13:09:25 +0100

    more on Tomcat/Weld

--- sandbox-tomcat-embedded/src/test/java/de/lbe/sandbox/tomcat/EmbeddedTomcatTest.java
+++ sandbox-tomcat-embedded/src/test/java/de/lbe/sandbox/tomcat/EmbeddedTomcatTest.java
@@ -1,30 +1,24 @@
 package de.lbe.sandbox.tomcat;
 
-import java.io.File;
 import java.util.Set;
 
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.CDI;
 
-import org.apache.catalina.Wrapper;
 import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.deploy.ApplicationListener;
 import org.apache.catalina.deploy.ContextResource;
 import org.apache.catalina.deploy.ContextTransaction;
-import org.apache.catalina.startup.Tomcat;
 import org.apache.naming.factory.BeanFactory;
-import org.apache.naming.resources.VirtualDirContext;
 import org.jboss.weld.resources.ManagerObjectFactory;
 import org.junit.Test;
 
 import com.atomikos.icatch.jta.UserTransactionFactory;
 import com.atomikos.icatch.jta.UserTransactionManager;
 
+import de.lbe.sandbox.tomcat.testapp.TestService;
 import de.asideas.lib.commons.cdi.BeanManagerUtils;
 import de.asideas.lib.commons.test.junit.AbstractJUnit4Test;
-import de.lbe.sandbox.tomcat.testapp.TestService;
-import de.lbe.sandbox.tomcat.weld.WeldListener;
 
 /**
  * @author lars.beuster
@@ -34,47 +28,13 @@ public class EmbeddedTomcatTest extends AbstractJUnit4Test {
 	@Test
 	public void testStart() throws Exception {
 
-		String webappDirLocation = "src/main/webapp/";
-		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(8080);
-		tomcat.enableNaming();
-
-		final StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-		// tomcat.addServlet(ctx, "jersey", new JerseyServlet());
-		// ctx.start();
-
-		ApplicationListener appListener = new ApplicationListener(WeldListener.class.getName(), false);
-		ctx.addApplicationListener(appListener);
-
-		// declare an alternate location for your "WEB-INF/classes" dir:
-		File additionWebInfClasses = new File("target/test-classes");
-		VirtualDirContext resources = new VirtualDirContext();
-		resources.setExtraResourcePaths("/WEB-INF/classes=" + additionWebInfClasses);
-		ctx.setResources(resources);
-
-		configureTransactions(ctx);
-
-		// doesn't work because binds to java:comp/env/BeanManager
-		configureCDI(ctx);
-
-		// String namingContextName = ctx.getNamingContextListener().getName();
-
-		// Context ctx = tomcat.addWebapp("/", new File(".").getAbsolutePath());
-		// ((StandardJarScanner) ctx.getJarScanner()).setScanAllFiles(true);
-
-		// FilterDef def = new FilterDef();
-		// def.setFilterClass(Listener.class.getName());
-		// ctx.addFilterDef(def);
-
-		Wrapper wrapper = Tomcat.addServlet(ctx, "test", new BindBeanManagerServlet(ctx));
-		wrapper.setLoadOnStartup(1);
-
+		TomcatBooter tomcat = new TomcatBooter();
+		tomcat.setWebappDir("src/main/webapp");
+		tomcat.setExtraClassesDir("target/test-classes");
 		tomcat.start();
-		// ctx.addServletMapping("/U", "test");
-		if (!ctx.getState().isAvailable()) {
-			return;
-		}
-		tomcat.getServer().await();
+
+		// configureTransactions(ctx);
+
 		//
 		// EmbeddedTestTomcat tomcat = new EmbeddedTestTomcat();
 		//
