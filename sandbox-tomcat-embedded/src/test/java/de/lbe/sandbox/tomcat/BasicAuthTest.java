30bd295387a3e5dedceb0883ff1de93a76bb9fd3
Lars Beuster <Lars.Beuster@asideas.de>
2014-05-08 22:17:12 +0200

    added embedded tomcat module

---
+++ sandbox-tomcat-embedded/src/test/java/de/lbe/sandbox/tomcat/BasicAuthTest.java
@@ -0,0 +1,16 @@
+package de.lbe.sandbox.tomcat;
+
+import org.junit.Test;
+
+/**
+ * @author lars.beuster
+ */
+public class BasicAuthTest extends AbstractEmbeddedTomcatTest {
+
+	protected TestTomcat tomcat;
+
+	@Test
+	public void testStartup() {
+		// put your breakpoint here
+	}
+}
