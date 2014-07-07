137b373f1a5076ee1c5d2926ce60cd64014751a5
Lars Beuster <Lars.Beuster@asideas.de>
2014-07-07 10:37:33 +0200

    reasteasy + spring

--- sandbox-resteasy3-spring/src/test/java/de/lbe/sandbox/resteasy/spring/TestTomcat.java
+++ sandbox-resteasy3-spring/src/test/java/de/lbe/sandbox/resteasy/spring/TestTomcat.java
@@ -1,5 +1,9 @@
 package de.lbe.sandbox.resteasy.spring;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
 import de.asideas.lib.commons.tomcat.embedded.EmbeddedTomcat;
 
 /**
@@ -7,7 +11,9 @@ import de.asideas.lib.commons.tomcat.embedded.EmbeddedTomcat;
  */
 public class TestTomcat extends EmbeddedTomcat {
 
-	private TestTomcat() {
+	@Override
+	protected void initContext() throws ServletException, IOException {
+		super.initContext();
 		addServletListener(ResteasyBootstrapListener.class);
 		addServletListener(Spring4ContextLoaderListener.class);
 		addContextParam("contextConfigLocation", "classpath:context-sandbox.xml");
