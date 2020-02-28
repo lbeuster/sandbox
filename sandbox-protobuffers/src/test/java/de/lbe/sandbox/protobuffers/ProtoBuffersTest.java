ccd3785fdb3403ffa38901ecb731d3fa2bd59ac2
Lars Beuster <l.beuster@cartelsol.com>
2020-02-28 14:34:53 +0100

    protobuf

--- sandbox-protobuffers/src/test/java/de/lbe/sandbox/protobuffers/ProtoBuffersTest.java
+++ sandbox-protobuffers/src/test/java/de/lbe/sandbox/protobuffers/ProtoBuffersTest.java
@@ -1,16 +1,19 @@
 package de.lbe.sandbox.protobuffers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
-import com.cartelsol.cate.shared.cl.client.NestedObject;
-import com.cartelsol.cate.shared.cl.client.TestRequest;
 import com.cartelsol.commons.lib.test.junit.AbstractJUnitTest;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import com.google.protobuf.util.JsonFormat.Parser;
 import com.google.protobuf.util.JsonFormat.Printer;
+
+import de.lbe.sandbox.protobuf.NestedObject;
+import de.lbe.sandbox.protobuf.TestPerson;
+import de.lbe.sandbox.protobuf.TestRequest;
 
 /**
  * @author lbeuster
@@ -53,5 +56,34 @@ public class ProtoBuffersTest extends AbstractJUnitTest {
         assertEquals(nested1.getId(), nested2.getId());
         assertEquals(nested1.getName(), nested2.getName());
 
+    }
+
+    @Test
+    public void test_required_json() throws InvalidProtocolBufferException {
+
+        String invalid = JsonFormat.printer().print(TestPerson.newBuilder().build());
+        System.out.println(invalid);
+
+        Parser parser = JsonFormat.parser();
+        TestPerson.Builder builder = TestPerson.newBuilder();
+        parser.merge(invalid, builder);
+        TestPerson person = builder.build();
+
+        assertEquals("", person.getName());
+        assertFalse(person.hasField(TestPerson.getDescriptor().findFieldByNumber(TestPerson.NAME_FIELD_NUMBER)));
+        assertEquals(0, person.getAge());
+        assertFalse(person.hasField(TestPerson.getDescriptor().findFieldByNumber(TestPerson.AGE_FIELD_NUMBER)));
+    }
+
+    @Test
+    public void test_required_binary() throws InvalidProtocolBufferException {
+
+        byte[] bytes = TestPerson.newBuilder().build().toByteArray();
+        TestPerson person = TestPerson.parseFrom(bytes);
+
+        assertEquals("", person.getName());
+        assertFalse(person.hasField(TestPerson.getDescriptor().findFieldByNumber(TestPerson.NAME_FIELD_NUMBER)));
+        assertEquals(0, person.getAge());
+        assertFalse(person.hasField(TestPerson.getDescriptor().findFieldByNumber(TestPerson.AGE_FIELD_NUMBER)));
     }
 }
